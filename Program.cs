1292c19e5dd706a7ef317880f6a22708e9e287fc
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-11 20:44:55 -0300

    Exercício Completo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("---- Tabuada ----");
+Console.ForegroundColor = ConsoleColor.Red;
+System.Console.Write("Digite o número desejado para saber a tabuada do mesmo: ");
+
+
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+int multiplo = 1;
+int resultado;
+
+Console.ForegroundColor = ConsoleColor.White;
+while ( multiplo <= 10)
+{
+    resultado = numeroDigitado * multiplo;
+    System.Console.WriteLine($"{numeroDigitado} X {multiplo} = {resultado}");
+    multiplo++;
+}
+Console.ForegroundColor = ConsoleColor.Green;
+System.Console.Write("Pressione uma tecla para fechar...");
+Console.ReadKey();
+Console.ResetColor();
